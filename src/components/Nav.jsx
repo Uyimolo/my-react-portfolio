4d4325c939843aa05c3d605e3c3119a3fe074d46
uyimolo <uyijustindavid@gmail.com>
2023-06-29 09:38:14 +0100

    fix:font-size of Home page font for mobile devices

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = ({ navActive }) => {
         navActive
           ? "translate-y-20 h-screen"
           : "-translate-y-[1000px] lg:translate-x-0"
-      } flex flex-col absolute right-0 bg-[#10101A] w-screen top-24 shadow-xl overflow-hidden transition-all duration-700 lg:relative lg:top-0 lg:flex-row lg:space-y-0 lg:space-x-8 lg:shadow-none lg:rounded-r lg:rounded-full  lg:pr-6 lg:pl-12 lg:py-4 lg:h-auto lg:translate-y-0 lg:bg-white lg:justify-end lg:w-fit lg:items-center`}
+      } flex flex-col absolute right-0 left-0 bg-[#10101A] w-screen top-24 shadow-xl overflow-hidden transition-all duration-700 lg:relative lg:top-0 lg:flex-row lg:space-y-0 lg:space-x-8 lg:shadow-none lg:rounded-r lg:rounded-full  lg:pr-6 lg:pl-12 lg:py-4 lg:h-auto lg:translate-y-0 lg:bg-white lg:justify-end lg:w-fit lg:items-center`}
     >
       {navItems.map((navItem, index) => (
         <Link
