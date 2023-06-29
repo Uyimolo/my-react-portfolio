42bf332f39a05054dc65a637d8daa317a5dc03b8
uyimolo <uyijustindavid@gmail.com>
2023-06-29 08:13:14 +0100

    feat: added typewriter effect to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
-    <div className="p-6 flex flex-col space-y-12">
+    <div className="p-6 flex flex-col space-y-12 pb-20">
       <h1 className="text-5xl text-white font-bold max-w-xl md:m-auto md:text-center mb-12">
         empowering businesses with{" "}
-        <h1 className="bg-gradient-to-r from-blue-900 via-indigo-500 to-red-600 inline-block text-transparent bg-clip-text">
-          pixel-perfect
-        </h1>{" "}
-        solutions.
+        <div className="font-bold bg-gradient-to-r from-blue-900 via-indigo-500 to-red-600 w-fit text-transparent bg-clip-text">
+          <Typewriter
+            options={{
+              strings: ["secure", "responsive", "optimized", "pixel-perfect"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+        solutions
       </h1>
 
       <div className="flex justify-between border p-2 rounded">
