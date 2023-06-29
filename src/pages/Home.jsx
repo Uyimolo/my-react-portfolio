0ad5d508df6e059b97818a2e8aedd79f0193846c
uyimolo <uyijustindavid@gmail.com>
2023-06-29 09:11:02 +0100

    made home responsive and fixed nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,15 @@
 import { Link } from "react-router-dom";
-import Typewriter from "typewriter-effect";
-
+import TypewriterHome from "../components/TypewriterHome";
 const Home = () => {
   return (
     <div className="p-6 flex flex-col space-y-12 pb-20">
-      <h1 className="text-5xl text-white font-bold max-w-xl md:m-auto md:text-center mb-12">
+      <h1 className="text-4xl text-gray-200 font-bold max-w-sm mb-12 md:m-auto md:text-center md:mb-8 lg:mt-12 lg:max-w-lg md:text-5xl">
         empowering businesses with{" "}
-        <div className="font-bold bg-gradient-to-r from-blue-900 via-indigo-500 to-red-600 w-fit text-transparent bg-clip-text">
-          <Typewriter
-            options={{
-              strings: ["secure", "responsive", "optimized", "pixel-perfect"],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </div>
-        solutions
+       <TypewriterHome />
+        solutions.
       </h1>
 
-      <div className="flex justify-between border p-2 rounded">
+      <div className="flex justify-between border border-gray-200 p-2 rounded max-w-md md:mx-auto md:w-80">
         <Link to="works" className="text-gray-200">
           See works
         </Link>
