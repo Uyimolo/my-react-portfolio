6c2c53c889fc94a613a58d11d7ddce0fbf332eef
uyimolo <uyijustindavid@gmail.com>
2023-07-01 19:06:38 +0100

    added tech stack and brief intro

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,32 +1,16 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
 
 import TypewriterHome from "../components/TypewriterHome";
+import MildCta from "./MildCta";
 const Landing = () => {
-  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <section className=" px-6 flex flex-col space-y-12 pt-4 md:px-16">
-      <h1 className="text-[45px] leading-[1.1] text-white font-bold max-w-sm mt-6  mb-8 md:text-left md:text-6xl md:mb-8  lg:mt-12 md:max-w-xl lg:text-7xl">
+      <h1 className="text-[45px] leading-[1.1] text-gray-200 font-bold max-w-sm mt-6  mb-8 md:text-left md:text-6xl md:mb-8  lg:mt-12 md:max-w-xl lg:text-7xl">
         empowering businesses with <TypewriterHome />
         solutions.
       </h1>
 
-      <div className="w-fit ">
-        <Link
-          to="works"
-          className="text-gray-200 text-xl"
-          onMouseOver={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          See works
-        </Link>
-        <div
-          className={`h-[2px] mt-2 bg-white transition-all duration-700 ${
-            isHovered ? "w-full" : "w-12"
-          } `}
-        ></div>
-      </div>
+      <MildCta text="See works" />
     </section>
   );
 };
