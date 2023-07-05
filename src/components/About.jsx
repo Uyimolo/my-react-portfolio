4ad7e719517f8ab3d0d9c061a7c789b1e942827a
uyimolo <uyijustindavid@gmail.com>
2023-07-05 17:18:56 +0100

    fixed text alignment for about and projects components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,8 @@ import MildCta from "./MildCta";
 const About = () => {
   return (
     <section className="px-6 flex flex-col space-y-4 h-full pb-12 md:px-16  xl:px-28">
-      <h2 className="text-2xl text-gray-200 lg:text-3xl lg:text-center">Introductions</h2>
-      <p className="text-gray-400 md:max-w-2xl lg:max-w-3xl lg:text-xl lg:text-center lg:mx-auto">
+      <h2 className="text-2xl text-gray-200 md:text-center lg:text-3xl ">Introductions</h2>
+      <p className="text-gray-400 md:max-w-2xl md:text-center md:mx-auto lg:max-w-3xl lg:text-xl ">
         I am Uyioghosa Justin Urhoghide (Uyi to friends and business
         associates). I am a front end web developer. I love creating websites
         that are not just visually appealing but functional and interactive.
